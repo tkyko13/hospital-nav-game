16c05ef9b69b9b77d1aef8d311cb36682d518839
tkyko13 <tkyk.o13@gmail.com>
2023-03-28 22:44:31 +0900

    commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Player's speed
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x,y,0);
+    }
+}
